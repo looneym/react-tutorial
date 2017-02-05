778bafd397fa9a66bef0fa9ea010528bf4789fc4
Micheál Looney <hello@micheallooney.com>
2017-02-05 00:15:52 +0000

    L31 - Searching for videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,17 +11,18 @@ class SearchBar extends Component {
 
   render () {
     return (
-      <div className="search-bar" >
+      <div className="search-bar">
       <input
         value={this.state.term}
-        onChange={event => this.setState({ term: event.target.value})}
+        onChange={event => this.onInputChange(event.target.value)}
         />
       </div>
     )
   }
 
-  onInputChange(event) {
-    this.setState({ term: event.target.value });
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 
 }
